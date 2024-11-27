6572a479c9f5b09dba470434542cc01d5a744896
vikakulyaa <viktoriaalekxeeva@gmail.com>
2024-11-27 14:08:27 +0200

    11

--- static/js/top.js
+++ static/js/top.js
@@ -10,6 +10,34 @@ laiks = adrese[2];
 let datums = new Date();
 let datumsVirkne = datums.getDate()+'.'+datums.getMonth()+'.'+datums.getFullYear()+'.'
 
+async function iegutDatusNoApi(url)
+{
+  let datiNoServera = await fetch(url);
+  let datiNoServeraJson = await datiNoServera.json();
+  return datiNoServeraJson;
+}
+
+async function atlasitTop();
+{
+  iegutDatusNoApi('cels pie datiem json')
+  let topsJson = await iegutDatusNoApi('topData');
+  console.log(datiJson);
+  for (i=0; i < topsJson.lenght; i++)
+  {
+    let tabula = document.querySelector(".tops");
+
+    tabula.innerHTML = tabula.innerHTML+`
+    <tr id="` +topsJson[i]['id']+`">
+     <td>  `+topsJson[i]['vards']+` </td>
+     <td>  `+topsJson[i]['klikski']+` </td>
+     <td>  `+topsJson[i]['laiks']+` </td>
+     <td>  `+topsJson[i]['datums']+` </td>
+    </tr>`;
+ }
+}
+atlasitTop();
+
+
 function pievienotTop() {
   let tabula = document.querySelector('.tops');
   tabula.innerHTML = tabula.innerHTML +`
